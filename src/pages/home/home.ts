d63fd3dcdb5bee81db283df099ef80335b75c084
ROBERT DONNELLY - STUDENT <G00358931@gmit.ie>
2019-03-06 17:48:09 +0000

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ConnaughtPage } from '../connaught/connaught';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,21 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  openConnaughtPage(){
+    this.navCtrl.push('ConnaughtPage');
+  }
+  openMunsterPage(){
+    this.navCtrl.push('MunsterPage');
+
+  }
+  openUlsterPage(){
+    this.navCtrl.push('UlsterPage');
+
+  }
+  openLeinsterPage(){
+    this.navCtrl.push('LeinsterPage');
+
+  }
 
 }
+
